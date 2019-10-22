09b669e399ff26421b804b7a1f9511e239dfe261
Alisson Santos de Souza <alisson.souza@ctis.com.br>
2019-10-21 22:55:22 -0300

    criando componente home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { PrivateModule } from './private/private.module';
+import { PublicModule } from './public/public.module';
+import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core/core.module';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularMaterialModule } from './shared/shared-modules/angular-material/angular-material.module';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CoreModule,
+    PrivateModule,
+    PublicModule,
+    SharedModule,
+    RouterModule,
+    AngularMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
